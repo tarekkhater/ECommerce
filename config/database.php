bda0184c8f3f82685b46f83cd9e59c05bc0276ab
tarekkhater <tarekkhater103@gmail.com>
2022-10-23 14:42:14 +0200

    update

--- config/database.php
+++ config/database.php
@@ -72,7 +72,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => $DATABASE_URL['host'],
             'port' => $DATABASE_URL['port'],
-            'database' => ltrim($DATABASE_URL['path'],'/'),
+            'database' => Ltrim($DATABASE_URL['path'],'/'),
             'username' => $DATABASE_URL['user'],
             'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
